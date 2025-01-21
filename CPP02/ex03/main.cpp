cb179da740e55d25b168f86ff0fbdc14f34f9cb7
Hieu Trung Tran <hitran@c1r1p10.hive.fi>
2025-01-21 14:58:56 +0200

    Inited ex03 by copying ex02 and creating Point functions and header.

---
+++ CPP02/ex03/main.cpp
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hitran <hitran@student.hive.fi>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/13 21:35:38 by hitran            #+#    #+#             */
+/*   Updated: 2025/01/21 14:35:13 by hitran           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+#include <iostream>
+
+int main( void ) {
+Fixed a;
+Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+Fixed c;
+c = Fixed(b.toFloat() * 0.124f);
+Fixed const d(Fixed(c.toFloat() / 0.1f));
+
+std::cout << ( c / a)  << std::endl;
+std::cout << ( a / b)  << std::endl;
+
+std::cout << a << std::endl;
+std::cout << ++a << std::endl;
+std::cout << a << std::endl;
+std::cout << a++ << std::endl;
+std::cout << a << std::endl;
+
+std::cout << b << std::endl;
+
+std::cout << c << std::endl;
+std::cout << d << std::endl;
+
+std::cout << Fixed::max( a, b ) << std::endl;
+std::cout << Fixed::min( a, c ) << std::endl;
+
+std::cout << (a > b)  << std::endl;
+
+return 0;
+}
