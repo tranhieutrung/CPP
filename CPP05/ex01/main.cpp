c954169e748f02d2161ac8fb994d2ba434015f67
tranhieutrung <tranhieutrung@outlook.com>
2025-03-07 09:56:27 +0200

    CPP05/ex01/main: Added a test for beSigned function.

--- CPP05/ex01/main.cpp
+++ CPP05/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hitran <hitran@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/04 09:34:17 by hitran            #+#    #+#             */
-/*   Updated: 2025/03/06 11:25:28 by hitran           ###   ########.fr       */
+/*   Updated: 2025/03/07 09:54:55 by hitran           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,9 +68,31 @@ int main()
 	std::cout << form2 << std::endl;
 	std::cout << YELLOW << "bur2 tries to sign form2: " << RESET << std::endl;
 	bur2.signForm(form2);
-	std::cout  << YELLOW << "Form2's information after being signed by bur1: " << RESET << std::endl;
+	std::cout  << YELLOW << "Form2's information after being signed by bur2: " << RESET << std::endl;
 	std::cout << form2 << std::endl;
 
+	std::cout << std::endl << BLUE << "Form2: Test beSigned only: " << RESET << std::endl;
+	std::cout << GREEN << "Form3's Constructors: " << RESET << std::endl;
+	Form		form3("Form3", 50, 50);
+	std::cout << GREEN << "Form3's information: " << RESET  << std::endl;
+	std::cout << form3 << std::endl;	std::cout << YELLOW << "bur1 tries to sign form3: " << RESET << std::endl;
+	try {
+		form3.beSigned(bur1);
+		std::cout << GREEN << "Form3 was signed." << RESET << std::endl;
+		std::cout  << YELLOW << "Form3's information after being signed by bur1: " << RESET << std::endl;
+		std::cout << form3 << std::endl;
+	} catch (const std::exception &e) {
+		std::cerr << e.what() <<std::endl;
+	}
+	std::cout << YELLOW << "bur2 tries to sign form3: " << RESET << std::endl;
+	try {
+		form3.beSigned(bur2);
+		std::cout << GREEN << "Form3 was signed." << RESET << std::endl;
+		std::cout  << YELLOW << "Form3's information after being signed by bur2: " << RESET << std::endl;
+		std::cout << form3 << std::endl;
+	} catch (const std::exception &e) {
+		std::cerr << e.what() <<std::endl;
+	}	
 	std::cout << std::endl << RED << "Deconstructors:" << RESET << std::endl;
 	return (0);
 }
