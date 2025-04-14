26e6f76052a1ca19f53f1f222c27d592e4e41fce
tranhieutrung <tranhieutrung@outlook.com>
2025-04-14 09:53:02 +0300

    CPP06/ex01: Added tests into main.

--- CPP06/ex01/main.cpp
+++ CPP06/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hitran <hitran@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/26 14:32:18 by hitran            #+#    #+#             */
-/*   Updated: 2025/04/10 14:49:11 by hitran           ###   ########.fr       */
+/*   Updated: 2025/04/14 09:50:13 by hitran           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,28 @@ int main() {
 	data.number = 10;
 	data.string = "Hello world!";
 
+	std::cout << "Original data:" << std::endl;
+	std::cout << "number = " << data.number << std::endl;
+	std::cout << "string = " << data.string << std::endl;
+
 	uintptr_t ptr = Serializer::serialize(&data);
 
 	Data *newData = Serializer::deserialize(ptr);
 
+	std::cout << std::endl << "Derived data:" << std::endl;
 	std::cout << "number = " << newData->number << std::endl;
 	std::cout << "string = " << newData->string << std::endl;
 
+	newData->number = 20;
+	newData->string = "Bye bye!";
+
+	std::cout << std::endl << "Derived data after modifying:" << std::endl;
+	std::cout << "number = " << newData->number << std::endl;
+	std::cout << "string = " << newData->string << std::endl;
+	
+	std::cout << std::endl << "Original data after modifying:" << std::endl;
+	std::cout << "number = " << data.number << std::endl;
+	std::cout << "string = " << data.string << std::endl;
+	
 	return (0);
 }
