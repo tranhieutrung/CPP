285ac91057794ba8d87c629a7ce2ad8937de2bfb
tranhieutrung <tranhieutrung@outlook.com>
2025-04-25 10:22:46 +0300

    CPP07/ex01: Update Makefile and code to follow the C++98 standard and work with evaluation sheet's test.

--- CPP07/ex01/iter.hpp
+++ CPP07/ex01/iter.hpp
@@ -6,26 +6,30 @@
 /*   By: hitran <hitran@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/26 14:26:39 by hitran            #+#    #+#             */
-/*   Updated: 2025/04/14 14:09:02 by hitran           ###   ########.fr       */
+/*   Updated: 2025/04/25 10:19:49 by hitran           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
-
 # include <iostream>
 # include <string>
 
-template <typename T, typename F>
-void iter(T *array, size_t length, F function) {
-	if (!array) {
-		return;
-	}
-	for (size_t index = 0; index < length; index++) {
-		function(array[index]);
-	}
-}
+// template <typename T, typename F>
+// void iter(T *array, size_t length, F function) {
+// 	if (!array) {
+// 		return;
+// 	}
+// 	for (size_t index = 0; index < length; index++) {
+// 		function(array[index]);
+// 	}
+// }
 
-template <typename T> 
-void print(T &para) {
-	std::cout << para << std::endl;
+template <typename T>
+void iter(T *array, size_t length, void (*function)(T const &)) {
+    if (!array) {
+        return;
+    }
+    for (size_t index = 0; index < length; index++) {
+        function(array[index]);
+    }
 }
--- CPP07/ex01/main.cpp
+++ CPP07/ex01/main.cpp
@@ -6,21 +6,45 @@
 /*   By: hitran <hitran@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/26 14:32:18 by hitran            #+#    #+#             */
-/*   Updated: 2025/04/14 14:10:17 by hitran           ###   ########.fr       */
+/*   Updated: 2025/04/25 10:16:05 by hitran           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "iter.hpp"
 
-int main( void ) {
+class Awesome
+{
+	public:
+		Awesome( void ) : _n( 42 ) { return; }
+		int get( void ) const { return this->_n; }
+	private:
+		int _n;
+};
+
+std::ostream & operator<<( std::ostream & o, Awesome const & rhs ) { o << rhs.get(); return o; }
+
+template< typename T >
+void print( T const & x ) { std::cout << x << std::endl; return; }
+
+int main() {
+	// Test 1:
+	std::cout << "Test 1: " << std::endl;
 	int num[] = {1, 2, 3, 4, 5};
 	std::string str[] = {"Hello", "42", "World", "!"};
 
 	std::cout << "Test with integer array:" << std::endl;
-	iter(num, 5, print<int>);
+	iter(num, 5, print);
 
 	std::cout << std::endl << "Test with string array:" << std::endl;
-	iter(str, 4, print<std::string>);
+	iter(str, 4, print);
 
+	// Test 2:
+	std::cout << std::endl << "Test 2: " << std::endl;
+	int tab[] = { 0, 1, 2, 3, 4 }; // <--- I never understood why you can't write int[] tab. Wouldn't that make more sense?
+	
+	Awesome tab2[5];
+	iter( tab, 5, print );
+	iter( tab2, 5, print );
+	
 	return 0;
 }
